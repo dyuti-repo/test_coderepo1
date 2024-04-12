5e4245fab3996bec375f7c3e295d5028e9a78516
dyuti-repo <134058520+dyuti-repo@users.noreply.github.com>
2024-04-12 17:38:05 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,36 +1,22 @@
-
 const express = require('express')
+const mongoose = require('mongoose')
 
-const app = express()                    //initialising app with express
+const url = 'mongodb://localhost/AlienDBex'
+const app = express()
 
-app.get('/',function(req,res){          //fetching data of "/" from server
-    res.send("Hello World")             //res.send bcoz as this is sent from server so response & send to client...
+mongoose.connect(url,{useNewurlParser:true})
+
+const con = mongoose.connection
+
+con.on('open',function(){
+    console.log('Connected.........')
 })
 
-//--------------------------------------------------------------
-//fetching different url information from server
+app.use(express.json())
 
-app.get('/alien',function(req,res){                
-    const id = req.query.id
-    res.send("Welcome back Alien " + id)
+const alienRouter = require('./routes/aliens')
+app.use('/aliens',alienRouter)
+
+app.listen(9000,function(){
+    console.log('Server Started')
 })
-
-app.get('/alien/:id',function(req,res){     //:id also.known.as url parameter ; ? a.k.a url query string 
-    const id = req.params.id               //This describes when in any browser client search in search bar if requests for alien whose id =20
-    if (id == 20)
-    {
-        res.send("Hey Pattanaik " + id)
-    }
-    else if (id == 21)
-    {
-        res.send("Hey Naveen " + id)       //Show the page to the client from server alien whose id =20 or21 etc 
-    }                                      // (possible using params & query string ?)
-               
-})
-
-//--------------------------------------------------------------
-//starting a server
-app.listen(9000,function(req,res){
-    console.log("Running....")
-})    // starting the server at port 9000
-
