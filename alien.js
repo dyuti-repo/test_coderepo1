e3de3787ffbe58091f89678771a21e3592c57414
dyuti-repo <134058520+dyuti-repo@users.noreply.github.com>
2024-04-12 17:34:03 +0530

    Add files via upload

---
+++ alien.js
@@ -0,0 +1,21 @@
+//schema for database for the module (alien) a.k.a MODELS
+const mongoose = require('mongoose')
+
+const alienSchema = new mongoose.Schema({
+
+    name:{
+        type:String,
+        required:true
+    },
+    tech: {
+        type:String,
+        required:true
+    },
+    sub: {
+        type:Boolean,
+        required:true,
+        default:false
+    }
+})
+
+module.exports = mongoose.model('Alien',alienSchema)
